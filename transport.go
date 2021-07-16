3456e7c472cebf5237994894553b8f8e50ce2049
haochen233 <252087791@qq.com>
2021-07-16 18:08:03 +0800

    add udp Connection to guard udp relay close.

--- transport.go
+++ transport.go
@@ -1,56 +1,81 @@
 package socks5
 
 import (
-	"io"
 	"net"
 	"strings"
 	"sync"
+	"time"
 )
 
 // Transporter transmit data between client and dest server.
 type Transporter interface {
-	TransportTCP(client net.Conn, remote net.Conn) error
-	TransportUDP(server *net.UDPConn, request *Request) error
+	TransportTCP(client *TCPConn, remote *TCPConn) error
+	TransportUDP(server *UDPConn, request *Request) error
 }
 
 type transport struct {
-	BufSize int
-	errCh   chan error
+	// IdleTimeout is the maximum duration for reading from socks client.
+	// it's only effective to socks server handshake process.
+	//
+	// If zero, there is no timeout.
+	IdleTimeout time.Duration
 }
 
-const maxLenOfDataGram = 65507
+const maxLenOfDatagram = 65507
 
-var transportPoll = &sync.Pool{
+var transportPool = &sync.Pool{
 	New: func() interface{} {
-		return make([]byte, maxLenOfDataGram)
+		return make([]byte, maxLenOfDatagram)
 	},
 }
 
 // TransportTCP use io.CopyBuffer transmit data.
-func (t *transport) TransportTCP(client net.Conn, remote net.Conn) error {
-	if t.errCh == nil {
-		t.errCh = make(chan error, 2)
-	}
+func (t *transport) TransportTCP(client *TCPConn, remote *TCPConn) error {
+	defer client.Close()
+	defer remote.Close()
+	errCh := make(chan error, 2)
 
-	f := func(dst net.Conn, src net.Conn) {
-		inBuf := make([]byte, t.BufSize)
-		_, err := io.CopyBuffer(dst, src, inBuf)
-		if err != nil {
-			if tcpRead, ok := src.(*net.TCPConn); ok {
-				tcpRead.CloseRead()
+	f := func(dst *TCPConn, src *TCPConn) {
+		ticker := time.NewTicker(t.IdleTimeout)
+		defer ticker.Stop()
+		buf := transportPool.Get().([]byte)
+		defer transportPool.Put(buf)
+
+		for {
+			select {
+			default:
+				n, err := src.rawConn.Read(buf)
+				if err != nil {
+					errCh <- err
+					return
+				}
+				src.SetState(StateActive)
+
+				n, err = dst.rawConn.Write(buf[:n])
+				if err != nil {
+					errCh <- err
+					return
+				}
+				dst.SetState(StateActive)
+				ticker.Reset(t.IdleTimeout)
+			case <-ticker.C:
+				dst.SetState(StateIdle)
+				src.SetState(StateIdle)
+				ticker.Reset(t.IdleTimeout)
+			case <-dst.CloseChan():
+				errCh <- nil
+				return
+			case <-src.CloseChan():
+				errCh <- nil
+				return
 			}
-			if tcpWrite, ok := dst.(*net.TCPConn); ok {
-				tcpWrite.CloseWrite()
-			}
-			t.errCh <- err
 		}
-		t.errCh <- nil
 	}
 	go f(remote, client)
 	go f(client, remote)
 
 	for i := 0; i < 1; i++ {
-		err := <-t.errCh
+		err := <-errCh
 		if err != nil {
 			return err
 		}
@@ -59,7 +84,7 @@ func (t *transport) TransportTCP(client net.Conn, remote net.Conn) error {
 }
 
 // TransportUDP forwarding UDP packet between client and dest.
-func (t *transport) TransportUDP(server *net.UDPConn, request *Request) error {
+func (t *transport) TransportUDP(server *UDPConn, request *Request) error {
 	// Client udp address, limit access to the association.
 	clientAddr, err := request.Address.UDPAddr()
 	if err != nil {
@@ -68,58 +93,71 @@ func (t *transport) TransportUDP(server *net.UDPConn, request *Request) error {
 
 	// Record dest address, limit access to the association.
 	forwardAddr := make(map[*net.UDPAddr]struct{})
-	buf := transportPoll.Get().([]byte)
+	buf := transportPool.Get().([]byte)
+	defer transportPool.Put(buf)
+	ticker := time.NewTicker(t.IdleTimeout)
+	defer ticker.Stop()
 
+	defer server.Close()
 	for {
-		// Receive data from remote.
-		n, addr, err := server.ReadFromUDP(buf)
-		if err != nil {
-			return err
-		}
-
-		// Should unpack data when data from client.
-		if strings.EqualFold(clientAddr.String(), addr.String()) {
-			destAddr, payload, err := UnpackUDPData(buf[:n])
+		select {
+		default:
+			// Receive data from remote.
+			n, addr, err := server.ReadFromUDP(buf)
 			if err != nil {
 				return err
 			}
+			server.SetState(StateActive)
 
-			destUDPAddr, err := destAddr.UDPAddr()
-			if err != nil {
-				return err
-			}
-			forwardAddr[destUDPAddr] = struct{}{}
+			// Should unpack data when data from client.
+			if strings.EqualFold(clientAddr.String(), addr.String()) {
+				destAddr, payload, err := UnpackUDPData(buf[:n])
+				if err != nil {
+					return err
+				}
 
-			// send payload to dest address
-			_, err = server.WriteTo(payload, destUDPAddr)
-			if err != nil {
-				return err
-			}
-		}
+				destUDPAddr, err := destAddr.UDPAddr()
+				if err != nil {
+					return err
+				}
+				forwardAddr[destUDPAddr] = struct{}{}
 
-		// Should pack data when data from dest client
-		if _, ok := forwardAddr[addr]; ok {
-			address, err := ParseAddress(addr.String())
-			if err != nil {
-				return err
-			}
-
-			// packed Data
-			packedData, err := PackUDPData(address, buf[:n])
-			if err != nil {
-				return err
+				// send payload to dest address
+				_, err = server.WriteToUDP(payload, destUDPAddr)
+				if err != nil {
+					return err
+				}
 			}
 
-			// send payload to client
-			_, err = server.WriteTo(packedData, clientAddr)
-			if err != nil {
-				return err
+			// Should pack data when data from dest host
+			if _, ok := forwardAddr[addr]; ok {
+				address, err := ParseAddress(addr.String())
+				if err != nil {
+					return err
+				}
+
+				// packed Data
+				packedData, err := PackUDPData(address, buf[:n])
+				if err != nil {
+					return err
+				}
+
+				// send payload to client
+				_, err = server.WriteToUDP(packedData, clientAddr)
+				if err != nil {
+					return err
+				}
 			}
+			ticker.Reset(t.IdleTimeout)
+		case <-ticker.C:
+			server.SetState(StateIdle)
+			ticker.Reset(t.IdleTimeout)
+		case <-server.CloseChan():
+			return nil
 		}
 	}
 }
 
 var DefaultTransporter Transporter = &transport{
-	BufSize: 1024,
-	errCh:   nil,
+	IdleTimeout: 30 * time.Second,
 }
